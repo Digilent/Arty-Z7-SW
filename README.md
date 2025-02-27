25793ef9850ded432997ff524250c4537f019c86
bog739 <bogdan.stanese@digilent.ro>
2025-02-27 21:15:20 +0200

    Notes :: Vitis mig v2024.1     - This branch has ready to use workspaces for some projects (see README.md)     - checkout.py script can be used for now

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-Root commit
+
+# Arty Z7 - Vitis Migration v2024.1
+
+## Workspaces
+
+* ws_20250227084436 - 20/HDMI-in/master, This project is ready to be used.
+* ws_20250227082129 - 10/HDMI-in/master, This project is ready to be used.
+* ws20250130013906 - 10/OOB/master, This project is ready to be used.
+* ws20250130014930 - 20/OBB/master, This project is ready to be used.
+
+## Scripts
+
+* checkout.py - This file was run from cmd-line: `vitis -s checkout.py`
---
+++ ws_20250227084436/hdmi_in_wrapper/ps7_cortexa9_0/domain_ps7_cortexa9_0/bsp/libsrc/build_configs/gen_bsp/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,11 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "")
+  file(REMOVE_RECURSE
+  "D:\\PrjMig-Vitis_2024.1\\repos\\arty-z7\\sw\\ws_20250227084436\\hdmi_in_wrapper\\ps7_cortexa9_0\\domain_ps7_cortexa9_0\\bsp\\include\\sleep.h"
+  "D:\\PrjMig-Vitis_2024.1\\repos\\arty-z7\\sw\\ws_20250227084436\\hdmi_in_wrapper\\ps7_cortexa9_0\\domain_ps7_cortexa9_0\\bsp\\include\\xiltimer.h"
+  "D:\\PrjMig-Vitis_2024.1\\repos\\arty-z7\\sw\\ws_20250227084436\\hdmi_in_wrapper\\ps7_cortexa9_0\\domain_ps7_cortexa9_0\\bsp\\include\\xtimer_config.h"
+  "D:\\PrjMig-Vitis_2024.1\\repos\\arty-z7\\sw\\ws_20250227084436\\hdmi_in_wrapper\\ps7_cortexa9_0\\domain_ps7_cortexa9_0\\bsp\\lib\\libxiltimer.a"
+  )
+endif()
